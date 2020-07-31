b05634514fe2a7844dfdcde7aab09a74ba1122e4
mathcard <matheus.carmo@hotmail.com>
2020-07-31 01:33:07 -0300

    Alterando scripts para realizar deploy no heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import PageDefault from '../../../components/PageDefault';
-import FormField/*, { FormFieldArrea }*/ from '../../../components/FormField';
-import Button from '../../../components/Button';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function  CadastrarCategoria(){
+function CadastrarCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  }
-  
+  };
+
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
-   setCategorias([...categorias, values]);   
-   setValues(valoresIniciais)
+    setCategorias([...categorias, values]);
+    setValues(valoresIniciais);
   }
 
-  function setValue(chave, valor){
+  function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor, // nome: 'Matheus'
-    })
+    });
   }
 
-  function handleChange(infosDoEvento){
-    //const { getAttribute, value } = infosDoEvento.target;   NÃO FUNCIONOU
-    const { name, value } = infosDoEvento.target;                
+  function handleChange(infosDoEvento) {
+    // const { getAttribute, value } = infosDoEvento.target;   NÃO FUNCIONOU
+    const { name, value } = infosDoEvento.target;
     setValue(name, value);
   }
 
@@ -42,77 +42,77 @@ function  CadastrarCategoria(){
           ...resposta,
         ]);
       });
-    
-    
-    //setTimeout(() => {
+
+    // setTimeout(() => {
     //  setCategorias([
     //    ...categorias,
     //    {
     //      "id": 1,
     //      "nome" : "Front End",
     //      "descricao" : "Uma categoria bacana",
-    //      "cor": "#cbd1ff" 
+    //      "cor": "#cbd1ff"
     //    },
     //    {
     //      "id": 2,
     //      "nome" : "Back End",
     //      "descricao" : "Uma categoria bacana",
-    //      "cor": "#cbd133"     
+    //      "cor": "#cbd133"
     //    },
     //  ]);
-    //}, 4 * 1000 );
+    // }, 4 * 1000 );
   }, []);
 
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria: {values.nome}</h1>
+      <h1>
+        Cadastrar Categoria:
+        {values.nome}
+      </h1>
       <form onSubmit={handleSubmit}>
 
         <FormField
-          label="Nome da Categoria: " 
-          type="text"          
+          label="Nome da Categoria: "
+          type="text"
           name="nome"
           placeholder="Nome"
           value={values.nome}
           onChange={handleChange}
-        /> 
+        />
 
         <FormField
-          label="Descrição da Categoria: "           
+          label="Descrição da Categoria: "
           name="descricao"
-          type="textarea"    
+          type="textarea"
           value={values.descricao}
           onChange={handleChange}
-        /> 
-    
+        />
+
         <FormField
           label="Cor"
           type="color"
           name="cor"
           value={values.cor}
           onChange={handleChange}
-        /> 
-        
+        />
+
         <Button>Cadastrar</Button>
       </form>
 
       {categorias.length === 0 && (<div>Loading...</div>) }
-        
-        <ul>
-          {categorias.map((categoria, indice) => {            
-            return(              
-              <li key={`${categoria}${indice}`}>
-                {categoria.nome} 
-              </li>                            
-            ) 
-          })}
-        </ul> 
+
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
       <Link to="/">
         Ir para Home
       </Link>
     </PageDefault>
-    //<div></div>
+    // <div></div>
   );
 }
 
